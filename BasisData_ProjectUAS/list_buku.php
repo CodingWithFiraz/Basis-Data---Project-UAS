23ae9982d20ff9a58c7e24ca3744c7f1aebb068a
Ipan12345-gns <136448325+Ipan12345-gns@users.noreply.github.com>
2024-12-22 13:40:32 +0700

    Tambah radio

--- BasisData_ProjectUAS/list_buku.php
+++ BasisData_ProjectUAS/list_buku.php
@@ -205,6 +205,28 @@
                 <p>Mahasiswa</p>
                 <h2> “Wahh bukunya keren-keren, banyak juga bukunya sehingga gw bisa banyak buku, apalagi yang teknologi
                     digital.”</h2>
+
+                <!-- Radio -->
+                <div class="radio-input-ipan">
+                    <label>
+                        <input value="value-1" name="value-radio" id="value-1" type="radio" checked="">
+                        <span>1</span>
+                    </label>
+                    <label>
+                        <input value="value-2" name="value-radio" id="value-2" type="radio">
+                        <span>2</span>
+                    </label>
+                    <label>
+                        <input value="value-3" name="value-radio" id="value-3" type="radio">
+                        <span>3</span>
+                    </label>
+                    <label>
+                        <input value="value-3" name="value-radio" id="value-3" type="radio">
+                        <span>4</span>
+                    </label>
+                    <span class="selection"></span>
+                </div>
+                <!-- Radio -->
             </div>
         </div>
 
